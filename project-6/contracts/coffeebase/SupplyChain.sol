300bf59ea26ad4335a8c9378dd2110e393370c67
GMil3az <gmil3az@gmail.com>
2020-01-18 19:17:08 +0700

    Modify all Roles, and make SupplyChain contract inherits from roles

--- project-6/contracts/coffeebase/SupplyChain.sol
+++ project-6/contracts/coffeebase/SupplyChain.sol
@@ -1,6 +1,13 @@
 pragma solidity ^0.4.24;
+
+// Import Roles
+import "../coffeeaccesscontrol/ConsumerRole.sol";
+import "../coffeeaccesscontrol/DistributorRole.sol";
+import "../coffeeaccesscontrol/FarmerRole.sol";
+import "../coffeeaccesscontrol/RetailerRole.sol";
+
 // Define a contract 'Supplychain'
-contract SupplyChain {
+contract SupplyChain is ConsumerRole, DistributorRole, FarmerRole, RetailerRole {
 
   // Define 'owner'
   address owner;
